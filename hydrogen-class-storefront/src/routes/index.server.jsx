688d693aa5eef6fbad23e0c47e1598e556db29fa
Sagar Virani <sagarvirani@gmail.com>
2023-08-29 13:41:37 +0530

    hydrogen demo project code

---
+++ hydrogen-class-storefront/src/routes/index.server.jsx
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout.server';
+
+export default function Home() {
+  return (
+    <Layout>
+      <div className="home-page container">
+        <h1>Homepage</h1>
+      </div>
+    </Layout>
+  );
+}
